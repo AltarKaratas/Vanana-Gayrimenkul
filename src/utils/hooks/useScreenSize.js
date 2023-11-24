ddbb132ef42cd3733db99f58d221877a16203a3b
AltarKaratas <altar.karatas@gmail.com>
2023-11-24 21:58:25 +0300

    deneme

--- src/utils/hooks/useScreenSize.js
+++ src/utils/hooks/useScreenSize.js
@@ -1,24 +1,26 @@
 import { useState, useEffect } from "react";
 
 const useScreenSize = () => {
-    const [screenSize,setScreenSize] = useState({
-        width: window.innerWidth,
-        height: window.innerHeight,
-    })
+  const [screenSize, setScreenSize] = useState({
+    width: window?.innerWidth || 0,
+    height: window?.innerHeight || 0,
+  });
+  console.log(window);
+  useEffect(() => {
+    const handleResize = () => {
+      if (typeof window !== undefined) {
+        setScreenSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+    };
+    window.addEventListener("resize", handleResize);
 
-    useEffect(() => {
-        const handleResize = () => {
-            setScreenSize({
-                width:window.innerWidth,
-                height:window.innerHeight,
-            });
-        }
-        window.addEventListener("resize",handleResize);
-
-        return () => {
-            window.removeEventListener("resize",handleResize);
-        }
-    },[])
-    return screenSize;
-}
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return screenSize;
+};
 export default useScreenSize;
