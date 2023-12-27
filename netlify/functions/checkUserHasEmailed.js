16910f07f698f5b6783625b8415a33e39fc9592d
AltarKaratas <altar.karatas@gmail.com>
2023-12-28 00:24:07 +0300

    Trying out serverless email func...

--- netlify/functions/checkUserHasEmailed.js
+++ netlify/functions/checkUserHasEmailed.js
@@ -12,7 +12,7 @@ exports.handler = async function (request, context) {
   });
   const isUser = await users.exists(ip);
 
-  if(isUser) {
+  if(false) {
     return {
       statusCode: 403,
     };
@@ -20,22 +20,28 @@ exports.handler = async function (request, context) {
     users.set(ip, "ip", { ex: 86400 });
     const sgMail = require("@sendgrid/mail");
     const emailObject = JSON.parse(request.body);
-    
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
-      to: "karatasaltar@gmail.com", // Change to your recipient
+      to: "altarkaratas@gmail.com", // Change to your recipient
       from: "karatasaltar@gmail.com", // Change to your verified sender
       subject: `${emailObject.firstName} adlı kullanıcının mesajı`,
       text: `Müşteri Tel No: ${emailObject.telephone} Email: ${emailObject.email} Mesajı: ${emailObject.userMessage}`,
+      html: `<strong>yarrrakkkkkkkkk</strong>`
     };
     try {
       const response = await sgMail.send(msg);
+      console.log("zürten");
 
       console.log(response);
+      console.log(response.body);
+      
+
+
       return {
         statusCode: [response[0].statusCode],
       };
     } catch (e) {
+      console.log("eldek");
       console.log(e);
       return {
         statusCode: 406,
