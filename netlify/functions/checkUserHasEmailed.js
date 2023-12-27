0ccbffffbba2ec43f4e8359af815fcdb4cd9297a
AltarKaratas <altar.karatas@gmail.com>
2023-12-27 18:59:59 +0300

    Serverless functions error handling check

--- netlify/functions/checkUserHasEmailed.js
+++ netlify/functions/checkUserHasEmailed.js
@@ -1,16 +1,18 @@
 exports.handler = async function (request, context) {
   const kv = require("@vercel/kv");
+  
+
   const ip = request.headers["x-forwarded-for"];
   
-  console.log(ip)
-  console.log(request)
+  
+  
   const users = kv.createClient({
     url: process.env.VANANAKV_REST_API_URL,
     token: process.env.VANANAKV_REST_API_TOKEN,
   });
   const isUser = await users.exists(ip);
 
-  console.log(!!isUser)
+ 
   if(isUser) {
     return {
       statusCode: 403,
@@ -19,7 +21,8 @@ exports.handler = async function (request, context) {
     users.set(ip, "ip", { ex: 86400 });
     const sgMail = require("@sendgrid/mail");
     const emailObject = JSON.parse(request.body);
-
+    console.log(emailObject)
+    console.log(request)
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
       to: "karatasaltar@gmail.com", // Change to your recipient
