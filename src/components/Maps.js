1bcfb32430f44ba862ca3256ce86ac111e609e1a
Altar <berkealtarkaratas@Berkes-MacBook-Pro.local>
2024-07-11 11:55:57 +0300

    Maps degisti

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -5,7 +5,7 @@ export default function Maps(props) {
       height={props.height}
       frameborder="0"
       referrerPolicy="no-referrer-when-downgrade"
-      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCw0xfUmKSWBMgcOBENHnuR4SHd1gwI7uQ&q=Azel+Kule,Ankara+Türkiye"
+      src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJc7Fc7ctI0xQRlw8QRvcn6u4&key=AIzaSyCEe0_C2RZOIBGHjQg4uhvj9vq-uF1HTkU"
       allowFullScreen
       className="brightness-75 mx-auto border-4 border_black_100"
     />
